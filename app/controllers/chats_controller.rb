3fe776f9014e495b01a2ecd0d9614d4171b5bbf8
Shujaat Azim <shujaat.azim@gmail.com>
2019-08-14 16:28:56 -0400

    added some new methods to the controllers to account for creating a new message obj, new user, and eventually deleting messages so as to avoid needing to drop db over and over to clear test messages

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,7 +2,28 @@ class ChatsController < ApplicationController
 
     def index
         @chats = Chat.all
-        render json: @chats.to_json(:include => [:messages, :users])
+        render json: @chats.to_json(:include => [:users, :messages])
+    end
+
+    def show
+        @chat = Chat.find(params[:id])
+        render json: @chat.to_json(:include => [:users, :messages])
+    end
+
+    def new
+        @chat = Chat.new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
     end
 
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,38 @@ class MessagesController < ApplicationController
 
     def index
         @messages = Message.all
-        render json: @messages.to_json(:include => :users)
+        render json: @messages.to_json(:include => [:chat, :user])
     end
+
+    def show
+        @message = Message.find(params[:id])
+        render json: @message.to_json(:include => [:chat, :user])
+    end
+
+    def new
+        @message = Message.new
+    end
+
+    def create
+        @message = Message.new(message_params)
+        @message.save
+        render json: @message
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:user_id, :chat_id, :content)
+    end
+
 
 end
